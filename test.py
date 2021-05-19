93d3a803e5de93a5760e61c2bdd8bfacfd99cdf7
ProfessorConfundus <83817060+ProfessorConfundus@users.noreply.github.com>
2021-05-19 21:20:04 +1000

    Adding comments to test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-import random
+"""
+What are you doing here? GET OUT OF HERE. >:P
+"""
+import random # imports random library
 
-for _ in range(10):
-  print(random.randint(0, 200)
+for _ in range(10): #repeats 10 times
+  print(random.randint(0, 200) # prints a random int between 0 and 200
 
-print("done")
+print("done") # prints "done" once the for loop is done
